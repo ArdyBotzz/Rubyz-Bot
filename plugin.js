ad078d6cfe7133eb8c780ae612b94275e46fb20d
Ardyyy <86863270+ArdyBotzz@users.noreply.github.com>
2022-07-11 11:56:30 +0800

    update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,5 +1,4 @@
-
-
+const { Message } = require("./func.js")
 
 module.export = {
   async handler(message, sock, store, setting) {
@@ -8,6 +7,7 @@ module.export = {
       let timestamp = new Date()
       let msg = message.messages[0]
       if (!msg.message) return;
+      let m = new Message(msg, sock, store)
       let type = Object.keys(msg.message)[0]
       let from = msg.key.remoteJid;
       let isGroup = from.endsWith("@g.us")
@@ -24,6 +24,46 @@ module.export = {
       let q = args.join(" ")
       let command = body.slice(0).trim().split(/ +/).shift().toLowerCase()
       let isOwner = !!setting.owner.find(o => o == sender)
+      let prefix = /^[°•π÷×¶∆£¢€¥®™✓_=|~!?#$%^&.+-,\/\\©^]/.test(command) ? command.match(/^[°•π÷×¶∆£¢€¥®™✓_=|~!?#$%^&.+-,\/\\©^]/gi) : '#'
+      let content = JSON.stringify(msg.message)
+      let isCmd = command.startsWith(prefix)
+      for (let name in global.plugins) {
+        let plugin = global.plugins[name]
+        if (!plugin) continue
+        if (plugin.disabled) continue
+        let isCommand = plugin.command instanceof RegExp ?
+          plugin.command.test(command) :
+          Array.isArray(plugin.command) ? 
+            plugin.command.some(cmd => cmd instanceof RegExp ? // RegExp in Array?
+                cmd.test(command) :
+                cmd === command
+              ) : typeof plugin.command == 'String' ? plugin.command === command : false
+        if (!isCommand) continue
+        let extra = {
+          type,
+          from,
+          isGroup,
+          sender,
+          metadata,
+          me,
+          isMeAdmin,
+          isAdmin,
+          pushname,
+          body,
+          args,
+          q,
+          command,
+          isOwner,
+          prefix,
+          content,
+          isCmd
+        }
+        try {
+          await plugin.call(sock, m, extra)
+        } catch (e) {
+          console.log(e)
+        }
+      }
     }
   }
 }
